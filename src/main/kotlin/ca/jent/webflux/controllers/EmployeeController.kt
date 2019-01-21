10caaacb7e00d601bf6d5de9ddddedc33e5b2381
Jocelyn Raymond <it.jraymond@gmail.com>
2019-01-21 14:23:27 -0700

    Added get employee endpoint

--- src/main/kotlin/ca/jent/webflux/controllers/EmployeeController.kt
+++ src/main/kotlin/ca/jent/webflux/controllers/EmployeeController.kt
@@ -3,6 +3,7 @@ package ca.jent.webflux.controllers
 import ca.jent.webflux.services.EmployeeService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -13,4 +14,7 @@ class EmployeeController {
 
     @GetMapping("/employees")
     fun getAllEmployees() = employeeService.getAllEmployees()
+
+    @GetMapping("/employees/{id}")
+    fun getEmployee(@PathVariable("id") id: String) = employeeService.getEmployee(id)
 }
--- src/main/kotlin/ca/jent/webflux/services/EmployeeService.kt
+++ src/main/kotlin/ca/jent/webflux/services/EmployeeService.kt
@@ -1,8 +1,10 @@
 package ca.jent.webflux.services
 
+import ca.jent.webflux.domain.Employee
 import ca.jent.webflux.repositories.EmployeeRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
 
 @Service
 class EmployeeService {
@@ -10,6 +12,8 @@ class EmployeeService {
     @Autowired
     lateinit var employeeRepository: EmployeeRepository
 
-    fun getAllEmployees() = employeeRepository.findAll()!!
+    fun getAllEmployees() = employeeRepository.findAll()
+
+    fun getEmployee(id: String): Mono<Employee> = employeeRepository.findById(id)
 
 }
